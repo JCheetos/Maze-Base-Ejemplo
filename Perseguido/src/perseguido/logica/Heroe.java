b06107d42fc9a1e07fac3285bdaceaa9e8584a69
JCheetos <yosoycheetos@outlook.com>
2018-05-03 07:18:06 -0500

    Movimiento tipo Bomberman, falta arreglar para que no atraviese paredes y evitar movimientos en diagonal

--- Perseguido/src/perseguido/logica/Heroe.java
+++ Perseguido/src/perseguido/logica/Heroe.java
@@ -15,92 +15,98 @@ import javax.swing.ImageIcon;
  * @author daza
  */
 public class Heroe implements Personaje {
+
     Image[][] imagenes = {{new ImageIcon(getClass().getResource("../imagenes/B1.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/B2.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/B3.png")).getImage()
-                          },
-                          {new ImageIcon(getClass().getResource("../imagenes/D1.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/D2.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/D3.png")).getImage()
-                          },
-                          {new ImageIcon(getClass().getResource("../imagenes/F1.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/F2.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/F3.png")).getImage()
-                          },
-                          {new ImageIcon(getClass().getResource("../imagenes/I1.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/I2.png")).getImage(),
-                           new ImageIcon(getClass().getResource("../imagenes/I3.png")).getImage()
-                          }
-                        };
+        new ImageIcon(getClass().getResource("../imagenes/B2.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/B3.png")).getImage()
+    },
+    {new ImageIcon(getClass().getResource("../imagenes/D1.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/D2.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/D3.png")).getImage()
+    },
+    {new ImageIcon(getClass().getResource("../imagenes/F1.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/F2.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/F3.png")).getImage()
+    },
+    {new ImageIcon(getClass().getResource("../imagenes/I1.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/I2.png")).getImage(),
+        new ImageIcon(getClass().getResource("../imagenes/I3.png")).getImage()
+    }
+    };
     int sentido = 2;
     int cont = 0;
     Image imagen = imagenes[sentido][cont];
-    int xPos = 32;
-    int yPos = 32;
+    int xPos = 40;
+    int yPos = 36;
     int xVel = 0;
     int yVel = 0;
 
     @Override
     public void draw(Graphics g) {
-        
+
         g.drawImage(imagen, xPos, yPos, null);
     }
-    
+
     @Override
-    public void update(){
+    public void update() {
         xPos += xVel;
         yPos += yVel;
-        cont ++;
+        cont++;
         cont = cont % 3;
         imagen = imagenes[sentido][cont];
-        
+
     }
 
     public void update(int c) {
         switch (c) {
             case 37:
                 sentido = 3;
-                xVel = -10;
-                yVel = 0;
+                xPos += -32;
+                yPos += 0;
                 break;
             case 38:
                 sentido = 0;
-                xVel = 0;
-                yVel = -10;
+                xPos += 0;
+                yPos += -32;
                 break;
             case 39:
                 sentido = 1;
-                xVel = 10;
-                yVel = 0;
+                xPos += 32;
+                yPos += 0;
                 break;
             case 40:
                 sentido = 2;
-                xVel = 0;
-                yVel = 10;
-
+                xPos += 0;
+                yPos += 32;
                 break;
         }
         System.out.println(c);
     }
+
     @Override
-    public void cambiarSentido(){
-        xVel *= -1;
-        yVel *= -1;
-        switch(sentido){
+    public void cambiarSentido() {
+//        xVel *= -1;
+//        yVel *= -1;
+        switch (sentido) {
             case 0:
-                sentido = 2;
+                xPos += 0;
+                yPos += 32;
                 break;
             case 1:
-                sentido = 3;
+                xPos += -32;
+                yPos += 0;
                 break;
             case 2:
-                sentido = 0;
+                xPos += 0;
+                yPos += -32;
                 break;
             case 3:
-                sentido = 1;
+                xPos += 32;
+                yPos += 0;
                 break;
         }
     }
+
     @Override
     public Rectangle getRect() {
         Rectangle rect = null;
