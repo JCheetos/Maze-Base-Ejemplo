0c9bf133dd7f18a4eeb13c04390a521ee036d20c
JCheetos <jsmendezv@correo.udistrital.edu.co>
2018-05-10 07:10:36 -0500

    Eventos Teclado Completo

--- JavaApplication7/src/javaapplication7/EventosTeclado.java
+++ JavaApplication7/src/javaapplication7/EventosTeclado.java
@@ -13,45 +13,63 @@ public class EventosTeclado extends JFrame implements KeyListener {
 
     public EventosTeclado() {
         super("Eventos del Teclado");
-        Container contenedor=getContentPane();
+        Container contenedor = getContentPane();
         contenedor.setLayout(new FlowLayout());
         linea = System.getProperty("line.separator");
-        texto=new JTextField(20);
-        informacion = new JTextArea(6,20);
+        texto = new JTextField(20);
+        informacion = new JTextArea(6, 20);
         informacion.setEditable(false);
         texto.addKeyListener(this);
-        principal=new JPanel();
+        principal = new JPanel();
         principal.add(texto);
         JScrollPane visualizacion = new JScrollPane(informacion);
         contenedor.add(principal, BorderLayout.NORTH);
         contenedor.add(visualizacion, BorderLayout.CENTER);
-        setSize(300,200);
+        setSize(300, 200);
         setVisible(true);
     }
-    public void keyTyped(KeyEvent e){
+
+    @Override
+    public void keyTyped(KeyEvent e) {
         char tecla = e.getKeyChar();
         int codigo = e.getKeyCode();
-        if (Character.isISOControl(tecla)){
+        if (Character.isISOControl(tecla)) {
             cadena = "Tecla pulsada = (caracter de control no imprimible)";
+        } else {
+            cadena = "Tecla pulsada = " + tecla + "'";
         }
-        else {
-            cadena = "Tecla pulsada = "+tecla+"'";
-        }
-        codigotecla="Codigo Tecla = " + codigo + "("+KeyEvent.getKeyText(codigo)+")";
-        informacion.append("EVENTO KEYTYPED: "+linea+ " "+cadena+linea+" "+codigotecla+linea);
+        codigotecla = "Codigo Tecla = " + codigo + "(" + KeyEvent.getKeyText(codigo) + ")";
+        informacion.append("EVENTO KEYTYPED: " + linea + " " + cadena + linea + " " + codigotecla + linea);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         EventosTeclado ventana = new EventosTeclado();
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        char tecla = e.getKeyChar();
+        int codigo = e.getKeyCode();
+        if (Character.isISOControl(tecla)) {
+            cadena = "Tecla pulsada = (caracter de control no imprimible)";
+        } else {
+            cadena = "Tecla pulsada = " + tecla + "'";
+        }
+        codigotecla = "Codigo Tecla = " + codigo + "(" + KeyEvent.getKeyText(codigo) + ")";
+        informacion.append("EVENTO KEYPRESSED: " + linea + " " + cadena + linea + " " + codigotecla + linea);
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        char tecla = e.getKeyChar();
+        int codigo = e.getKeyCode();
+        if (Character.isISOControl(tecla)) {
+            cadena = "Tecla pulsada = (caracter de control no imprimible)";
+        } else {
+            cadena = "Tecla pulsada = " + tecla + "'";
+        }
+        codigotecla = "Codigo Tecla = " + codigo + "(" + KeyEvent.getKeyText(codigo) + ")";
+        informacion.append("EVENTO KEYRELEASED: " + linea + " " + cadena + linea + " " + codigotecla + linea);
     }
 }
